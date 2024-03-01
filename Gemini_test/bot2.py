55f0bb8898b77578e4cd89d8ca6db90f9cba856e
Dhruv Chopra <50653715+dhruvchopra2003@users.noreply.github.com>
2024-03-02 03:24:37 +0530

    Prompt changes

--- Gemini_test/bot2.py
+++ Gemini_test/bot2.py
@@ -23,14 +23,13 @@ with open(csv_file_path, "r", newline="", encoding="utf-8") as file:
 initial_prompt = (
     "You are a real estate expert assisting customers with finding ONLY commercial properties in the NCR (National Capital Region) of India."
     "MAKE ALL YOUR CONVERSATIONS FEEL NATURAL AND HUMAN"
-    # "Before making any suggestion, Get to know the customer's requirements: ask about their prefered city, and any specific locality they are looking for first. Ask one question and wait for the user's answer, before asking the next"
-    # "Inform them that you might have some properties, then ask them if they are looking for long term or short term type investements, and if they are looking for plots, or office spaces or shops etc."
     "Before making suggestions, ask the user about these, one at a time,preferred city, preferred locality, long/short term returns, if they are looking for plots/offices/shops etc"
     f"Once they've answered these, check for properties in this to suggest exactly 5 most appropriate listings from {custom_data}, NEVER REPEAT LISTINGS"
-    "Prices of all lie in between 65lacs to 4Cr, chose and give any random number in between"
-    "Give a little background about the builder you suggest and some of their previous successful projects"
+    "Give small summarized answers, don't give bullets and subheaders, highlight the important part in the answer"
     "Give tables and images wherever possible"
-    "Narrow down the customer's choices, once the customer seems interested in a property, inquire how they want to be contacted, their name, number and city of residence"
+    "Give a little background about the builder you suggest and some of their previous successful projects"
+    "Give more information to user about the properties they are likely to chose." 
+    "Inquire how they want to be contacted, their name, number and city of residence"
     "Only answer real estate related questions"
 )
 
@@ -86,7 +85,7 @@ if user_input != None and user_input.strip() != "":
     messages.insert(0, {"role": "system", "content": initial_prompt})
 
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", messages=messages, max_tokens=600, temperature=0.4
+        model="gpt-3.5-turbo", messages=messages, max_tokens=600, temperature=0.6
     )
 
     chatgpt_reply = response.choices[0].message.content.strip()
